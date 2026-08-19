fb5eac34e55b1863f45bc6ab7455c3b2a52a46ba
opx <pleasureunderpressure@gmail.com>
2026-08-20 06:40:59 +0700

    sync

---
+++ samples/Opx.MudBlazor.FlatUi.Sample/Components/Pages/SpatialWorkItem.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 opx. All rights reserved.
+
+namespace Opx.MudBlazor.FlatUi.Sample.Components.Pages;
+
+internal sealed record SpatialWorkItem(
+    string Key,
+    string Title,
+    string Description,
+    string Status,
+    string Meta,
+    string Icon);
